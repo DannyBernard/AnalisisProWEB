44e57dcaba6cc3613447554e7563e788b911ea07
DANNY BERNARD <danny.bernard@hotmail.com>
2019-09-08 10:35:18 -0400

    avanzando 1.0

---
+++ ClassLibrary2/AnalisisRepositorio.cs
@@ -0,0 +1,62 @@
+﻿using BLL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+   public class AnalisisRepositorio : RepositorioBase<Analisis>
+    {
+        public override Analisis Buscar(int id)
+        {
+            Analisis analisis = new Analisis();
+            try
+            {
+                analisis = _contexto.analisis.Find(id);
+                analisis.detalle.Count();
+                foreach(var item in analisis.detalle)
+                {
+                    string s = item.Tipo.Descripcion;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return analisis;
+        }
+
+        public override bool Modificar(Analisis analisis)
+        {
+            try
+            {
+                var Anterior = _contexto.analisis.Find(analisis.AnalisisID);
+                foreach(var item in Anterior.detalle)
+                {
+                    if (!analisis.detalle.Exists(d => d.ID == item.ID))
+                    {
+                        item.Tipo = null;
+                        _contexto.Entry(item).State = System.Data.Entity.EntityState.Deleted;
+                    }
+                }
+                foreach(var item in analisis.detalle)
+                {
+
+                    var estado = item.ID > 0 ? EntityState.Modified;
+                    if (_contexto.SaveChanges() > 0)
+                    
+                        paso = true;
+                    
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
